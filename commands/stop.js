f23478b3584ab27d148f6491e9ad2da392e8e50b
Klairm <ivangardis@hotmail.com>
2021-08-14 23:28:51 +0200

    UPDATE: Update command to v13

--- commands/stop.js
+++ commands/stop.js
@@ -1,21 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 module.exports = {
-    name: "stop",
-    description: "Stop the player.",
-    usage: "-k stop ",
-    execute(message, args) {
-        if (!message.member.voice.channel) return message.channel.send("❌ | You are not in a voice channel!");
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send("❌ | You are not in my voice channel!");
-        if (!message.client.player.isPlaying(message)) {
-            return message.channel.send("❌ | I'm not playing anything?");
-        }
-        if (message.client.player.stop(message)) {
-            return message.channel.send("✅ | Stopped the player!");
-        } else {
-            return message.channel.send("❌ | Something went wrong trying to stop the player.");
-        }
+	data: new SlashCommandBuilder().setName('stop').setDescription('Stops the player'),
+	async execute(interaction) {
+		if (!interaction.member.voice.channel) return interaction.channel.send('❌ | You are not in a voice channel!');
+		if (interaction.guild.me.voice.channel && interaction.member.voice.channel.id !== interaction.guild.me.voice.channel.id)
+			return interaction.reply('❌ | You are not in my voice channel!');
 
-    },
+		if (!interaction.client.player.getQueue(interaction.guild)) {
+			console.log(interaction.client.player.getQueue(interaction.guild));
+			return interaction.reply("❌ | I'm not playing anything.");
+		}
 
-
-
+		interaction.client.player.getQueue(interaction.guild).stop(interaction);
+		return interaction.reply('✅ | Stopped the player!');
+	},
 };
