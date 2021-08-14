bd3f9fa998039027a453b403d8bf556592db1d5b
Klairm <ivangardis@hotmail.com>
2021-08-14 15:49:05 +0200

    UPDATE: Update commands to work with discordJS v13

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,26 +4,24 @@ const Discord = require('discord.js');
 const file = new Discord.MessageAttachment('assets/liK.png');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('avatar')
-    .setDescription('Displays user avatar')
-    .addUserOption((option) =>
-      option.setName('user').setDescription('The user').setRequired(true)
-    ),
-  async execute(interaction) {
-    if (!interaction.options.getMember('member'))
-      return interaction.reply({
-        content: "That member doesn't exists!",
-        ephemeral: true,
-      });
-    const embed = {
-      author: {
-        name: `${interaction.options.get('user').user.username}'s avatar`,
-      },
-      image: {
-        url: `${interaction.options.get('user').user.displayAvatarURL()}`,
-      },
-    };
-    interaction.reply({ files: [file], embeds: [embed], ephemeral: true });
-  },
+	data: new SlashCommandBuilder()
+		.setName('avatar')
+		.setDescription('Displays user avatar')
+		.addUserOption((option) => option.setName('user').setDescription('The user').setRequired(true)),
+	async execute(interaction) {
+		if (!interaction.options.getMember('member'))
+			return interaction.reply({
+				content: "That member doesn't exists!",
+				ephemeral: true,
+			});
+		const embed = {
+			author: {
+				name: `${interaction.options.get('user').user.username}'s avatar`,
+			},
+			image: {
+				url: `${interaction.options.get('user').user.displayAvatarURL()}`,
+			},
+		};
+		interaction.reply({ files: [file], embeds: [embed], ephemeral: true });
+	},
 };
