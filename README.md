8739977cc3eb63ce3cffe5be44230191d74490a8
Klairm <ivangardis@hotmail.com>
2021-08-15 00:20:01 +0200

    UPDATE: Updated to discordJS v13

--- README.md
+++ README.md
@@ -1,76 +1,82 @@
-# Klabot  ![alt text](https://i.imgur.com/cbEmj2G.png)
+# Klabot ![alt text](https://i.imgur.com/cbEmj2G.png)
 
 Klabot is a really useless Discord Bot created using DiscordJS and quickDB
 
-
 # Requirements
-  - NodeJS v12
-  - DiscordJS `npm install discord.js`
-  - QuickDB   `npm install quick.db`
-  - Discord-Player `npm install --save discord-player` + `npm install --save @discordjs/opus`
- 
- You also need a file named `config.json` with your token from the [Discord Developer Portal](https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot), also you can edit the prefix there, the file should look like this:
- ```json
- {
-	"prefix": "-k ",
+
+- NodeJS v12
+- DiscordJS `npm install discord.js @discordjs/rest discord-api-types @discordjs/voice sodium`
+- QuickDB `npm install quick.db`
+- Discord-Player `npm install --save discord-player ` + `npm install --save @discordjs/opus`
+
+You also need a file named `config.json` with your token from the [Discord Developer Portal](https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot),the file should look like this:
+
+```json
+{
 	"token": "YOUR_TOKEN_HERE"
 }
 ```
 
 For the assets you need to place them on a folder called assets, name the image as liK.png, or change the name on the source.
 Also you need a bell.mp3 file on the root for the bell sound, or again change the name on the source.
-# Usage 
-You can see a list of the commands using the help command `-k help`
+
+# Usage
+
+You need to register the slash commands to the client, for more info check the guide [Registering slash commands](https://discordjs.guide/interactions/registering-slash-commands.html#guild-commands)
 
 ## "Bell" System
-Klabot has a system that will play an audio on A voice channel when someone joins B voice channel, at the moment is not really useless, but in a future commands to 
-allow users join A voice channel  if it's unlocked.
-For use this, Klabot needs 2 voice channels: Channel where the .mp3 audio will be played ( A channel ),
-                                             and the channel where the bot will be looking if someone is in ( B channel)
-                                             
-For set this channels you can use the bell and door commands, example:
- ```
- -k door 692785363927695460
- -k bell 671100267592286220
- ```
- If you want to update the channel ID you can just run the door/bell command again.
- 
 
- You don't need to provide the channel ID in this case, just provide what type of channel you want to remove (bell or door). 
+Klabot has a system that will play an audio on A voice channel when someone joins B voice channel, at the moment is not really useless, but in a future commands to
+allow users join A voice channel if it's unlocked.
+For use this, Klabot needs 2 voice channels: Channel where the .mp3 audio will be played ( A channel ),
+and the channel where the bot will be looking if someone is in ( B channel)
+
+For set this channels you can use the bell and door commands, example:
+
+```
+/set door #voiceChannel
+/set bell #voiceChannel
+
+```
+
+If you want to update the channel ID you can just run the door/bell command again.
+
+You don't need to provide the channel ID in this case, just provide what type of channel you want to remove (bell or door).
 
 ## Favorite messages
 
 Klabot provides a way to send your favorite messages to a specified channel, to do this you need to specify the channel where all the favorite messages will be sent.
-You can do that with the command `-k favmessage channelID` 
+You can do that with the command `/set favmessage #channel`
 Example:
+
 ```
- -k favmessage 519969191067910144
+ /set favmessage #channel
 ```
+
 The favorite message are those that have an reaction with the pill emoji :pill:
 
 ## Removing channels ID
- If you want to remove the channels ID, you can use the deleteid command, example:
-```
- -k deleteid bell
- -k deleteid door
- -k deleteid favmessage
-```
- This will delete the channel ID of the key specified from the quick.db database.
 
-## Nickname "protection"
-With the command `-k nickname-protection on` you can enable the "nickname protection" which means that users can't use special characters on their nickname, if they try to do that, their nickname will be reset.
-For disable this: `-k nickname-protection off`
+If you want to remove the channels ID, you can use the deleteid command, example:
+
+```
+/unset bell
+/unset door
+/unset favmessage
+```
+
+This will delete the channel ID of the key specified from the quick.db database.
 
 ## Music Player
-You can reproduce music and YouTube videos with the player commands like: `-k play`, `-k skip`, `-k resume`, `-k stop`, `-k pause`.
+
+You can reproduce music and YouTube videos with the player commands like: `/play`, `/skip`, `/resume`, `/stop`, `/pause`.
 
 ## TODO:
- - Add a events creation system, so you can schedule reunions, game times, anything with your friends.
- - Add a lock and unlock for voice channels making the bell system more useful.
- - Add a WebHook for RSS.
- 
- 
- 
+
+- Add a events creation system, so you can schedule reunions, game times, anything with your friends.
+- Add a lock and unlock for voice channels making the bell system more useful.
+- Move from quickDB to MongoDB
+
  <p align=center>
  <img src="https://i.imgur.com/b6aCKA2.png"/>
  </p>
--- index.js
+++ index.js
@@ -1,129 +1,126 @@
 const fs = require('fs');
 const db = require('quick.db');
 const { Client, Collection, Intents } = require('discord.js');
+const { Player } = require('discord-player');
+const { createAudioPlayer, joinVoiceChannel, createAudioResource } = require('@discordjs/voice');
 
 const { token } = require('./config.json');
 const client = new Client({
-  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  ],
+	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.GUILD_VOICE_STATES,
+	],
 });
 client.commands = new Collection();
 
-const commandFiles = fs
-  .readdirSync('./commands')
-  .filter((file) => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
+const commandFolders = fs.readdirSync('./commands');
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+		client.commands.set(command.data.name, command);
+	}
 }
 
-/*** Player events for music 
+client.player = new Player(client);
 
-client.player.on("trackStart", (message, track) => message.channel.send(`✅ | Now playing ${track.title}...`));
+/*** Player events for music ***/
 
-client.player.on("trackAdd", (message, queue, track) => message.channel.send(`✅ | Added ${track.title} to the queue...`));
+client.player.on('trackStart', (queue, track) => queue.metadata.channel.send(`🎶 | Now playing **${track.title}**!`));
 
-client.player.on("playlistAdd", (message, queue, playlist) => message.channel.send(`✅ | Added ${playlist.title} to the queue...`));
+client.player.on('trackAdd', (queue, track) => queue.metadata.channel.send(`✅ | Added ${track.title} to the queue...`));
 
-client.player.on("error", (error, message) => console.log("Player Error -> ", error));
+client.player.on('playlistAdd', (queue, playlist) => queue.metadata.channel.send(`✅ | Added ${playlist.title} to the queue...`));
 
+client.player.on('error', (error) => console.log('Player Error -> ', error));
 
-
-client.player.on("noResults", (message, query) => message.channel.send("❌ | No results found."));
-***/
 client.once('ready', async () => {
-  var date = new Date();
-  console.log(`Ready at ${date}`);
+	var date = new Date();
+	console.log(`Ready at ${date}`);
 });
 
 client.on('messageReactionAdd', async (reaction, user) => {
-  if (reaction.partial) {
-    try {
-      await reaction.fetch();
-    } catch (error) {
-      return console.log('Something went wrong  fetching the message: ', error);
-    }
-  }
+	if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			return console.log('Something went wrong  fetching the message: ', error);
+		}
+	}
 
-  // Check if the emoji reaction that has been added is the :pill: emoji, if so create an embed with user information and send it to a channel
-  if (reaction.emoji.name == '💊') {
-    if (!db.has(`${reaction.message.guild.id}.favmessage`)) return;
-    // Thanks LilaQ
-    const favMessage = {
-      author: {
-        name: reaction.message.author.username,
-        icon_url: reaction.message.author.displayAvatarURL(),
-      },
-      fields: [
-        {
-          name: '#' + reaction.message.channel.name,
-          value: '[jump]' + '(' + reaction.message.url + ')',
-          inline: false,
-        },
-      ],
+	// Check if the emoji reaction that has been added is the :pill: emoji, if so create an embed with user information and send it to a channel
+	if (reaction.emoji.name == '💊') {
+		if (!db.has(`${reaction.message.guild.id}.favmessage`)) return;
+		// Thanks LilaQ
+		const favMessage = {
+			author: {
+				name: reaction.message.author.username,
+				icon_url: reaction.message.author.displayAvatarURL(),
+			},
+			fields: [
+				{
+					name: '#' + reaction.message.channel.name,
+					value: '[jump]' + '(' + reaction.message.url + ')',
+					inline: false,
+				},
+			],
 
-      url: reaction.message.url,
-      description: reaction.message.content,
-    };
+			url: reaction.message.url,
+			description: reaction.message.content,
+		};
 
-    reaction.message.guild.channels.cache
-      .get(db.get(`${reaction.message.guild.id}.favmessage`))
-      .send({
-        embeds: [favMessage],
-      });
-  }
+		reaction.message.guild.channels.cache.get(db.get(`${reaction.message.guild.id}.favmessage`)).send({
+			embeds: [favMessage],
+		});
+	}
 });
-
+const bellPlayer = createAudioPlayer();
 client.on('voiceStateUpdate', async (oldMember, newMember) => {
-  if (newMember.member.user.bot) return;
-  if (!db.has(`${newMember.guild.id}.bell`)) return;
-  if (!db.has(`${newMember.guild.id}.door`)) return;
+	if (newMember.member.user.bot) return;
+	if (!db.has(`${newMember.guild.id}.bell`)) return;
+	if (!db.has(`${newMember.guild.id}.door`)) return;
+	const resource = createAudioResource('assets/bell.mp3');
 
-  try {
-    var bell = await client.channels.cache
-      .get(db.get(`${newMember.guild.id}.bell`))
-      .join();
-    //client.channels.cache.get(db.get(`${newMember.guild.id}.bell`)).overwritePermissions([{id:newMember.guild.roles.everyone,deny: 'VIEW_CHANNEL',}]);
-    var door = db.get(`${newMember.guild.id}.door`);
-    let newUserChannel = newMember.channel;
-    if (newUserChannel == null && oldMember.channel == door)
-      console.log(`${newMember.member.displayName} left from the door`);
-    if (newUserChannel == door) {
-      var date = new Date();
-      console.log(
-        `${newMember.member.displayName} entered to the door that has channelID: ${newUserChannel} on  the guild: ${newMember.member.guild.name}, at time: ${date} `
-      );
-      console.log(`[INFO] Bell ID -> ${bell.channel.id}`);
-      console.log(`[INFO] Door ID -> ${door}`);
-      bell.play('assets/bell.mp3');
-    }
-  } catch (error) {
-    return console.log('Something went wrong,', error);
-  }
+	try {
+		const bell = joinVoiceChannel({
+			channelId: db.get(`${newMember.guild.id}.bell`),
+			guildId: newMember.guild.id,
+			adapterCreator: newMember.guild.voiceAdapterCreator,
+		});
+		const subscription = bell.subscribe(bellPlayer);
+
+		var door = db.get(`${newMember.guild.id}.door`);
+		let newUserChannel = newMember.channel;
+		if (newUserChannel == null && oldMember.channel == door) console.log(`${newMember.member.displayName} left from the door`);
+		if (newUserChannel == door) {
+			var date = new Date();
+			bellPlayer.play(resource);
+		}
+	} catch (error) {
+		return console.log('Something went wrong,', error);
+	}
 });
 
 client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
+	if (!interaction.isCommand()) return;
 
-  try {
-    await client.commands.get(interaction.commandName).execute(interaction);
-  } catch (error) {
-    console.error(error);
-    return interaction.reply({
-      content: 'There was an error while executing this command!',
-      ephemeral: true,
-    });
-  }
+	try {
+		await client.commands.get(interaction.commandName).execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({
+			content: '❌ | There was an error while executing this command!',
+			ephemeral: true,
+		});
+	}
 });
 
 client.on('error', async (error) => {
-  console.log('An error has ocurred', error);
+	console.log('❌ | An error has ocurred', error);
 });
 
 client.login(token);
