0ae20853306eec99861e1c652d97eaae4743a67e
Klairm <ivangardis@hotmail.com>
2022-08-11 00:40:42 +0200

    feat: add commands register script

---
+++ commands.js
@@ -0,0 +1,33 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord.js");
+const { token, clientId, guildId } = require("./config.json");
+const fs = require("node:fs");
+
+const commands = [];
+
+const commandFolders = fs.readdirSync("./commands");
+
+for (const folder of commandFolders) {
+  const commandFiles = fs
+    .readdirSync(`./commands/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    commands.push(command.data.toJSON());
+  }
+}
+const rest = new REST({ version: "10" }).setToken(token);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commands,
+    });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
