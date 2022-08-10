5d8eed94610c297ebb6460989f6faeebf14acf38
Klairm <ivangardis@hotmail.com>
2022-08-11 01:04:11 +0200

    fix[music-player]: change channel voice detection

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -4,7 +4,7 @@ module.exports = {
 	data: new SlashCommandBuilder().setName('pause').setDescription('Pauses the current track.'),
 	async execute(interaction) {
 		if (!interaction.member.voice.channel) return interaction.reply({ embeds:[{title: '❌ | You are not in a voice channel!'}], ephemeral: true });
-		if (interaction.guild.members.me.voice.channel && interaction.member.voice.channel.id !== interaction.guild.me.voice.channel.id)
+		if (interaction.member.voice.channel.id !== interaction.guild.members.me.voice.channel?.id )
 			return interaction.reply({ content: '❌ | You are not in my voice channel!', ephemeral: true });
 
 		if (!interaction.client.player.getQueue(interaction.guild.id)) {
--- commands/music/skip.js
+++ commands/music/skip.js
@@ -6,7 +6,7 @@ module.exports = {
 		if (!interaction.member.voice.channel) interaction.reply({ embeds:[{title: '❌ | You are not in a voice channel!'}], ephemeral: true });
 		
 		
-		if (interaction.guild.members.me.voice.channel && interaction.member.voice.channel.id !== interaction.guild.members.me.voice.channel.id)
+		if (interaction.member.voice.channel.id !== interaction.guild.members.me.voice.channel?.id )
 			return interaction.reply({ embeds:[{title: '❌ | You are not in my voice channel!'}], ephemeral: true });
 
 		if (!interaction.client.player.getQueue(interaction.guild.id)) {
