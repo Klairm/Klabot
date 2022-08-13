c43a3e3c4ddad011a8d04e021cce90e426140a6a
Klairm <ivangardis@hotmail.com>
2022-08-13 23:23:13 +0200

    refactor[set/unset]: clean code and rework functions

--- commands/config/set.js
+++ commands/config/set.js
@@ -1,24 +1,18 @@
-const { SlashCommandBuilder, ChannelType } = require("discord.js");
-const {QuickDB} = require("quick.db");
+const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require("discord.js");
+const { QuickDB } = require("quick.db");
 const db = new QuickDB();
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("set")
-    .setDescription(
-      "Set the channels for different usages, (favorite messages, door, bell"
-    )
+    .setDescription("Set the channels for different usages, (favorite messages, door, bell")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
     .addSubcommand((subcommand) =>
       subcommand
         .setName("favmessage")
-        .setDescription(
-          "Set the channel to send favorite message selected with the pill emoji"
-        )
+        .setDescription("Set the channel to send favorite message selected with the pill emoji")
         .addChannelOption((option) =>
-          option
-            .setName("favmessage")
-            .setDescription("Channel where the favorite message will be send")
-            .setRequired(true)
+          option.setName("favmessage").setDescription("Channel where the favorite message will be send").setRequired(true)
         )
     )
     .addSubcommand((subcommand) =>
@@ -26,12 +20,7 @@ module.exports = {
         .setName("door")
         .setDescription("Set the channel for the door")
         .addChannelOption((option) =>
-          option
-            .setName("door")
-            .setDescription(
-              "Channel where the bot will listen if anyone enters to sound activate the bell"
-            )
-            .setRequired(true)
+          option.setName("door").setDescription("Channel where the bot will listen if anyone enters to sound activate the bell").setRequired(true)
         )
     )
     .addSubcommand((subcommand) =>
@@ -39,93 +28,59 @@ module.exports = {
         .setName("bell")
         .setDescription("Set the channel where the bell will sound")
         .addChannelOption((option) =>
-          option
-            .setName("bell")
-            .setDescription(
-              "Channel where the bell will sound after someone joins the door."
-            )
-            .setRequired(true)
+          option.setName("bell").setDescription("Channel where the bell will sound after someone joins the door.").setRequired(true)
         )
     ),
   async execute(interaction) {
-    if (!interaction.member.permissions.has("MANAGE_CHANNELS"))
-      return interaction.reply({
-        content: "❌ | You don't have permissions to manage channels.",
-        ephemeral: true,
-      });
-
     switch (interaction.options.getSubcommand()) {
       case "favmessage":
-        if (setChannel(interaction, "favmessage")) {
-          return interaction.reply(
-            "✅ | Channel saved for favorite messages succesfully."
-          );
-        } else
-          return interaction.reply({
-            content: "❌ | Failed to save channel.",
-            ephemeral: true,
-          });
+        setChannel(interaction, "favmessage");
         break;
       case "door":
-        if (setChannel(interaction, "door")) {
-          return interaction.reply(
-            "✅ | Channel saved as door channel succesfully."
-          );
-        } else
-          return interaction.reply({
-            content: "❌ | Failed to save channel.",
-            ephemeral: true,
-          });
+        setChannel(interaction, "door");
         break;
       case "bell":
-        if (setChannel(interaction, "bell")) {
-          return interaction.reply(
-            "✅ | Channel saved as bell channel succesfully."
-          );
-        } else
-          return interaction.reply({
-            content: "❌ | Failed to save channel.",
-            ephemeral: true,
-          });
+        setChannel(interaction, "bell");
         break;
       default:
         interaction.reply({
           content: "❌ | Need to specify between favmessage, door OR bell.",
           ephemeral: true,
         });
-        break;
     }
   },
 };
 
 async function setChannel(interaction, name) {
-  if (
-    interaction.options.getChannel(name).type == ChannelType.GuildText &&
-    (name == "door" || name == "bell")
-  )
+  // TODO: Rework this function
+  // TODO:  handle duplication id's: there's shouldn't be 2 channels with the same ID
+  if (interaction.options.getChannel(name).type == ChannelType.GuildText && (name == "door" || name == "bell"))
     return interaction.reply({
-      content: "❌ | You selected a text channel, voice channel needed",
+      embeds: [{ title: "❌ | You selected a text channel, voice channel needed" }],
       ephemeral: true,
     });
-  if (
-    interaction.options.getChannel(name).type == ChannelType.GuildVoice &&
-    name == "favmessage"
-  )
+  if (interaction.options.getChannel(name).type == ChannelType.GuildVoice && name == "favmessage")
     return interaction.reply({
-      content: "❌ | You selected a voice channel, text channel needed",
+      embeds: [{ title: "❌ | You selected a voice channel, text channel needed" }],
       ephemeral: true,
     });
-  await db.set(
-    `${interaction.guild.id}.${name}`,
-    interaction.options.getChannel(name).id
-  );
+  await db.set(`${interaction.guild.id}.${name}`, interaction.options.getChannel(name).id);
   if (
-    await  db.has(`${interaction.guild.id}.${name}`) &&
-    await db.get(`${interaction.guild.id}.${name}`) ==
-      interaction.options.getChannel(name).id
+    (await db.has(`${interaction.guild.id}.${name}`)) &&
+    (await db.get(`${interaction.guild.id}.${name}`)) == interaction.options.getChannel(name).id
   ) {
-    return true;
+    return interaction.reply({
+      embeds: [
+        {
+          title: `✅ | Channel saved for ${name} succesfully.`,
+        },
+      ],
+      ephemeral: true,
+    });
   } else {
-    return false;
+    return interaction.reply({
+      embeds: [{ title: "❌ | Failed to save channel." }],
+      ephemeral: true,
+    });
   }
 }
--- commands/config/unset.js
+++ commands/config/unset.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, PermissionFlagsBits, ConnectionVisibility } = require("discord.js");
 const { QuickDB } = require("quick.db");
 const db = new QuickDB();
 
@@ -6,61 +6,47 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("unset")
     .setDescription("Unset channel from the usage.")
-    .addChannelOption((option) =>
-      option
-        .setName("channel")
-        .setDescription("Channel to delete configuration")
-        .setRequired(true)
-    ),
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+    .addChannelOption((option) => option.setName("channel").setDescription("Channel to delete configuration").setRequired(true)),
 
   async execute(interaction) {
-    if (!interaction.member.permissions.has("MANAGE_CHANNELS"))
+    let channel;
+
+    if (!(await db.has(`${interaction.guild.id}`))) {
       return interaction.reply({
-        content: "❌ | You don't have permissions to manage channels.",
+        embeds: [{ title: "❌ | There's no configuration avaible in this server." }],
         ephemeral: true,
       });
-    unsetChannel(interaction);
+    }
+    // TODO: This probably can be optimized
+    if (interaction.options.getChannel("channel").id == (await db.get(`${interaction.guild.id}.bell`))) {
+      channel = "bell";
+    } else if (interaction.options.getChannel("channel").id == (await db.get(`${interaction.guild.id}.door`))) {
+      channel = "door";
+    } else if (interaction.options.getChannel("channel").id == (await db.get(`${interaction.guild.id}.favmessage`))) {
+      channel = "favmessage";
+    } else {
+      return interaction.reply({
+        embeds: [
+          {
+            title: "❌ | This channel doesn't have any configuration saved. Maybe wrong channel selected?",
+          },
+        ],
+
+        ephemeral: true,
+      });
+    }
+
+    if (await db.delete(`${interaction.guild.id}.${channel}`)) {
+      return interaction.reply({
+        embeds: [{ title: " ✅ | Channel configuration removed succesfully." }],
+        ephemeral: true,
+      });
+    } else {
+      interaction.reply({
+        embeds: [{ title: "❌ | Failed to remove channel configuration." }],
+        ephemeral: true,
+      });
+    }
   },
 };
-
-async function unsetChannel(interaction) {
-  for (let i = 0; i < db.all().length; i++) {
-    if (db.all()[i].ID == interaction.guild.id) {
-      var id = db.all()[i].ID;
-      var values = db.all()[i].data;
-    }
-  }
-
-  if (Object.keys(values).length == 0) {
-    return interaction.reply({
-      content:
-        "❌ | This channel doesn't have any configuration saved. Maybe wrong channel selected?",
-      ephemeral: true,
-    });
-  }
-  if (id == undefined)
-    return interaction.reply({
-      content: "❌ | There's no configuration avaible in this server.",
-      ephemeral: true,
-    });
-
-  Object.keys(values).forEach((key) => {
-    if (values[key] == interaction.options.getChannel("channel").id) {
-      if (db.delete(`${interaction.guild.id}.${key}`)) {
-        return interaction.reply(
-          "✅ | Channel configuration removed succesfully."
-        );
-      } else {
-        return interaction.reply({
-          content: "❌ | Failed to remove channel configuration.",
-          ephemeral: true,
-        });
-      }
-    } else
-      return interaction.reply({
-        content:
-          "❌ | This channel doesn't have any configuration saved. Maybe wrong channel selected?",
-        ephemeral: true,
-      });
-  });
-}
