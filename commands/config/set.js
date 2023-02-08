ba362624edbabf37d4ddf7d3e0aafff3301cdae9
Klairm <ivangardis@hotmail.com>
2023-02-08 15:50:49 +0100

    feat(set.js): when setting the bell's channel, it will deny view permissions for everyone

--- commands/config/set.js
+++ commands/config/set.js
@@ -12,7 +12,10 @@ module.exports = {
         .setName("favmessage")
         .setDescription("Set the channel to send favorite message selected with the pill emoji")
         .addChannelOption((option) =>
-          option.setName("favmessage").setDescription("Channel where the favorite message will be send").setRequired(true)
+          option
+            .setName("favmessage")
+            .setDescription("Channel where the favorite message will be send")
+            .setRequired(true)
         )
     )
     .addSubcommand((subcommand) =>
@@ -20,7 +23,12 @@ module.exports = {
         .setName("door")
         .setDescription("Set the channel for the door")
         .addChannelOption((option) =>
-          option.setName("door").setDescription("Channel where the bot will listen if anyone enters to sound activate the bell").setRequired(true)
+          option
+            .setName("door")
+            .setDescription(
+              "Channel where the bot will listen if anyone enters to sound activate the bell"
+            )
+            .setRequired(true)
         )
     )
     .addSubcommand((subcommand) =>
@@ -28,7 +36,10 @@ module.exports = {
         .setName("bell")
         .setDescription("Set the channel where the bell will sound")
         .addChannelOption((option) =>
-          option.setName("bell").setDescription("Channel where the bell will sound after someone joins the door.").setRequired(true)
+          option
+            .setName("bell")
+            .setDescription("Channel where the bell will sound after someone joins the door.")
+            .setRequired(true)
         )
     ),
   async execute(interaction) {
@@ -51,36 +62,56 @@ module.exports = {
   },
 };
 
-async function setChannel(interaction, name) {
-  // TODO: Rework this function
-  // TODO:  handle duplication id's: there shouldn't be 2 channels with the same ID
-  if (interaction.options.getChannel(name).type == ChannelType.GuildText && (name == "door" || name == "bell"))
+
+function checkChannel(interaction, name){
+  if (
+    interaction.options.getChannel(name).type == ChannelType.GuildText &&
+    (name == "door" || name == "bell")
+  ) {
     return interaction.reply({
       embeds: [{ title: "❌ | You selected a text channel, voice channel needed" }],
       ephemeral: true,
     });
-  if (interaction.options.getChannel(name).type == ChannelType.GuildVoice && name == "favmessage")
+  }
+  if (interaction.options.getChannel(name).type == ChannelType.GuildVoice && name == "favmessage") {
     return interaction.reply({
       embeds: [{ title: "❌ | You selected a voice channel, text channel needed" }],
       ephemeral: true,
     });
-  await db.set(`${interaction.guild.id}.${name}`, interaction.options.getChannel(name).id);
-  if (
-    (await db.has(`${interaction.guild.id}.${name}`)) &&
-    (await db.get(`${interaction.guild.id}.${name}`)) == interaction.options.getChannel(name).id
-  ) {
-    return interaction.reply({
-      embeds: [
-        {
-          title: `✅ | Channel saved for ${name} succesfully.`,
-        },
-      ],
-      ephemeral: true,
-    });
-  } else {
-    return interaction.reply({
-      embeds: [{ title: "❌ | Failed to save channel." }],
-      ephemeral: true,
-    });
   }
+}
+
+async function setChannel(interaction, name) {
+  // TODO: Rework this function
+  // TODO:  handle duplication id's: there shouldn't be 2 channels with the same ID
+  checkChannel()
+
+  await db.set(`${interaction.guild.id}.${name}`, interaction.options.getChannel(name).id)
+    .then(async () => {
+      // Handle bell channel permissions: deny view channel permission for @everyone
+      if (name == "bell") {
+        await interaction.options.getChannel(name).edit({
+          permissionOverwrites: [
+            { id: interaction.guild.roles.everyone, deny: [PermissionFlagsBits.ViewChannel] },
+          ],
+        });
+      }
+    })
+    .then(() => {
+      return interaction.reply({
+        embeds: [
+          {
+            title: `✅ | Channel saved for ${name} succesfully.`,
+          },
+        ],
+        ephemeral: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return interaction.reply({
+        embeds: [{ title: "❌ | Failed to save channel." }],
+        ephemeral: true,
+      });
+    });
 }
